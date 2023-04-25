0b52a75f6445692442ffb77c4a63e00e0678ff94
Tam-BobManuel <bglorytb@gmail.com>
2023-04-25 13:52:51 -0700

    update _printf

--- _printf.c
+++ _printf.c
@@ -1,64 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
-
 /**
- * _printf - Works like the standard printf
- * @format: Pointer to format string
+ * _printf - receives input with parameters and print a formatted string
+ * @format: string for formatting input
  *
- * Return: number of characters printed (except null byte)
+ * Return: The total characters printed
  */
 int _printf(const char *format, ...)
 {
+	int result;
+	conver_t func[] = {{"c", print_char}, {"s", print_string},
+	{"%", print_percent}, {NULL, NULL}};
 	va_list args;
-	int num_chars = 0, i;
 
-	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-				return (-1)
 
-			switch (format[i])
-			{
-				case 'c':
-					num_chars += print_char(args);
-					break;
-				case 's':
-					num_chars += print_string(args);
-					break;
-				case 'd':
-				case 'i':
-					num_chars += print_number(args);
-					break;
-				case '%':
-					num_chars += _putchar('%');
-					break;
-				case 'b':
-					num_chars += print_binary(args);
-					break;
-				case 'o':
-					num_chars += print_octal(args);
-					break;
-				case 'x':
-					num_chars += print_hex(args);
-					break;
-				case 'X':
-					num_chars += print_hex_upper(args);
-					break;
-				case 'u':
-					num_char += print_unsigned(args);
-					break;
-				default:
-					break;
-			}
-		}
-		else
-			num_chars += _putchar(format[i]);
-	}
+	va_start(args, format);
+	result = process(format, func, args);
 	va_end(args);
-	return (num_chars);
+	return (result);
 }
--- main.h
+++ main.h
@@ -1,20 +1,46 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef PRINTF_H
+#define PRINTF_H
 
-#include <stdarg.h>
-#include <stddef.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
+int process(const char *format, conver_t func[], va_list args);
 int _printf(const char *format, ...);
-int print_string(va_list args);
-int print_number(va_list args);
 int _putchar(char c);
-int print_hex(va_list args);
-int print_hex_upper(va_list args);
-int print_octal(va_list args);
-int print_unsigned(va_list args);
-int print_octal(va_list args)
-int print_char(va_list args);
-int print_binary(va_list args, int count);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+/*Helper functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
+
 
 #endif
