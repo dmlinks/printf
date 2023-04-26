cd0367cb9708e6ddacd11b1cb06c43a508dcf2a7
Tam-BobManuel <bglorytb@gmail.com>
2023-04-25 17:13:40 -0700

    improved _printf

--- _printf.c
+++ _printf.c
@@ -1,32 +1,27 @@
 #include "main.h"
 
 /**
- * _printf - custom implementation of printf
+ * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
- *
- * This function produces output according to a format string containing
- * characters and conversion specifiers. It determines which printing function
- * to call depending on the conversion specifiers contained in the format string.
- * The function supports flags such as '-' (left justify), '+' (print sign),
- * and ' ' (print space before positive number), as well as width and precision
- * modifiers.
- *
- * Return: the number of characters printed (excluding the null byte), or
- *         -1 if there is an error
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
 	const char *p;
 	va_list arguments;
-	flags_t flags = {.minus = 0, .plus = 0, .space = 0};
-	int count = 0;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
 
 	va_start(arguments, format);
-
-	if (!format)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	for (p = format; *p; p++)
 	{
 		if (*p == '%')
@@ -40,17 +35,13 @@ int _printf(const char *format, ...)
 			while (get_flag(*p, &flags))
 				p++;
 			pfunc = get_print(*p);
-			if (!pfunc)
-				count += _putchar('%') + _putchar(*p);
-			else
-				count += pfunc(arguments, &flags);
-		}
-		else
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
 			count += _putchar(*p);
 	}
 	_putchar(-1);
-
 	va_end(arguments);
-
 	return (count);
 }
