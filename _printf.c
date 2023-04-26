84a04a5b0f5d72a639fc43a6e51806266a611008
Tam-BobManuel <bglorytb@gmail.com>
2023-04-25 17:03:00 -0700

    optimised _printf.c

--- _printf.c
+++ _printf.c
@@ -1,27 +1,32 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
+ * _printf - custom implementation of printf
  * @format: format string containing the characters and the specifiers
- * Description: this function will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ *
+ * This function produces output according to a format string containing
+ * characters and conversion specifiers. It determines which printing function
+ * to call depending on the conversion specifiers contained in the format string.
+ * The function supports flags such as '-' (left justify), '+' (print sign),
+ * and ' ' (print space before positive number), as well as width and precision
+ * modifiers.
+ *
+ * Return: the number of characters printed (excluding the null byte), or
+ *         -1 if there is an error
  */
 int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
 	const char *p;
 	va_list arguments;
-	flags_t flags = {0, 0, 0};
-
-	register int count = 0;
+	flags_t flags = {.minus = 0, .plus = 0, .space = 0};
+	int count = 0;
 
 	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
+
+	if (!format)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
+
 	for (p = format; *p; p++)
 	{
 		if (*p == '%')
@@ -35,13 +40,17 @@ int _printf(const char *format, ...)
 			while (get_flag(*p, &flags))
 				p++;
 			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
+			if (!pfunc)
+				count += _putchar('%') + _putchar(*p);
+			else
+				count += pfunc(arguments, &flags);
+		}
+		else
 			count += _putchar(*p);
 	}
 	_putchar(-1);
+
 	va_end(arguments);
+
 	return (count);
 }
